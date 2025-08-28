3faab38818c9852fe0209dbaa9a757c71beb37aa
bhoomika84awasthi <bhoomika84awasthi@gmail.com>
2025-08-28 20:57:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./app"; // <-- works whether App.js or App.jsx
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
