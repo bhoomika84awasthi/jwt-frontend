20fafce7adb71e3915f344dc36ce86d3efe8936f
bhoomika84awasthi <bhoomika84awasthi@gmail.com>
2025-08-28 20:47:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // <-- works whether App.js or App.jsx
+import App from "./app"; // <-- works whether App.js or App.jsx
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
