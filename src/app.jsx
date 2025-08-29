5471375aa6a1f83b5cacd7698e9dbf460a2a5c62
bhoomika84awasthi <bhoomika84awasthi@gmail.com>
2025-08-29 18:19:54 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import PrivateRoute from './components/PrivateRoute';
+import Signup from './pages/signup';
+import Login from './pages/login';
+import Dashboard from './pages/dashboard';
+import PrivateRoute from './components/privateRoute';
 
 function App() {
   return (
@@ -15,9 +15,9 @@ function App() {
       </nav>
 
       <Routes>
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
       </Routes>
     </div>
   );
