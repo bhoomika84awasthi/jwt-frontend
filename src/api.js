7f4c1ffc25d6f3248ad40eaea7a023417e26dfad
bhoomika84awasthi <bhoomika84awasthi@gmail.com>
2025-08-29 18:32:41 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://jwt-backend-1-funz.onrender.com", // 👈 Your Render backend
+  baseURL: import.meta.env.VITE_API_BASE_URL, // ✅ comes from .env
 });
 
-// Example API functions
+// Auth routes
 export const registerUser = (data) => API.post("/api/auth/register", data);
 export const loginUser = (data) => API.post("/api/auth/login", data);
 export const getProtected = () => API.get("/api/protected");
