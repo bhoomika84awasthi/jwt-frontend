e3de61e176d61f7301949dedf873109a0549a2db
bhoomika84awasthi <bhoomika84awasthi@gmail.com>
2025-08-29 18:18:52 +0530

    Update App.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import Signup from './pages/signup';
-import Login from './pages/login';
-import Dashboard from './pages/dashboard';
-import PrivateRoute from './components/privateRoute';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <div>
       <nav style={{ padding: '10px', background: '#eee' }}>
-        <Link to="/signup" style={{ marginRight: '10px' }}>Signup</Link>
-        <Link to="/login" style={{ marginRight: '10px' }}>Login</Link>
-        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/Signup" style={{ marginRight: '10px' }}>Signup</Link>
+        <Link to="/Login" style={{ marginRight: '10px' }}>Login</Link>
+        <Link to="/Dashboard">Dashboard</Link>
       </nav>
 
       <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
       </Routes>
     </div>
   );
