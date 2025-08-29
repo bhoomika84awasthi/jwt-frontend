4fd077bb483d0854e71d1925f37c223064d2f258
bhoomika84awasthi <bhoomika84awasthi@gmail.com>
2025-08-29 18:15:36 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
-import axios from 'axios';
+// api.js
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
- || 'http://localhost:5000/api'
+  baseURL: "https://jwt-backend-1-funz.onrender.com", // Render backend URL
 });
 
-export default API;
+// Example API calls
+export const loginUser = (data) => API.post("/api/login", data);
+export const registerUser = (data) => API.post("/api/register", data);
